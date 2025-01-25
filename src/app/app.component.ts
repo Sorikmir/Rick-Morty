a9979fbfcea60e8ef2ceb8c41179be59d18d8f82
Sorikmir <sergiimyr@gmail.com>
2025-01-25 15:37:48 -0500

    some new changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterLink, RouterOutlet],
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+}
